6fb665fe1eb6199b34f7618e6924e1b25f56a3e6
陈裕滨 <1378511140@qq.com>
2019-04-15 19:55:56 +0800

    修改了引用vuex bug

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-
+import store from './store/store'
 import '../static/wxui/wxui.css'
+// 使用vuex
+Vue.prototype.$store = store
 
 Vue.config.productionTip = false
 App.mpType = 'app'
